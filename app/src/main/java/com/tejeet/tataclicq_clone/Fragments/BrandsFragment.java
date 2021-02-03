39b8874f44c85b73f12cd17c9b308acfe6c71111
Tejeet <Tejeetm@gmail.com>
2021-02-03 12:10:01 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/tejeet/tataclicq_clone/Fragments/BrandsFragment.java
@@ -0,0 +1,22 @@
+package com.tejeet.tataclicq_clone.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tejeet.tataclicq_clone.R;
+
+public class BrandsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_brands, container, false);
+    }
+}
--- app/src/main/java/com/tejeet/tataclicq_clone/MainActivity.java
+++ app/src/main/java/com/tejeet/tataclicq_clone/MainActivity.java
@@ -1,14 +1,133 @@
 package com.tejeet.tataclicq_clone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.tejeet.tataclicq_clone.Fragments.BrandsFragment;
+import com.tejeet.tataclicq_clone.Fragments.CategoryFragment;
+import com.tejeet.tataclicq_clone.Fragments.HomeFragment;
+import com.tejeet.tataclicq_clone.Fragments.MyAccountFragment;
+import com.tejeet.tataclicq_clone.Fragments.MyBagFragment;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "tag";
+
+    private BottomNavigationView mBotoomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.ic_baseline_search_24);
+
+        mBotoomNavigation = (BottomNavigationView) findViewById(R.id.nav_view);
+
+        mBotoomNavigation.setSelectedItemId(R.id.navigation_home);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        mBotoomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                Fragment selctedFragment = null;
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.navigation_home:
+                        vibrateSense();
+                        getSupportActionBar().setTitle("Home");
+                        selctedFragment = new HomeFragment();
+                        break;
+
+                    case R.id.navigation_category:
+                        vibrateSense();
+                        getSupportActionBar().setTitle("Category");
+                        selctedFragment = new CategoryFragment();
+                        break;
+
+                    case R.id.navigation_Brands:
+                        vibrateSense();
+                        getSupportActionBar().setTitle("Brand");
+                        selctedFragment = new BrandsFragment();
+                        break;
+
+                    case R.id.navigation_myaccount:
+                        vibrateSense();
+                        getSupportActionBar().setTitle("My Account");
+                        selctedFragment = new MyAccountFragment();
+                        break;
+
+                    case R.id.navigation_mybag:
+                        vibrateSense();
+                        getSupportActionBar().setTitle("My Cart");
+                        selctedFragment = new MyBagFragment();
+                        break;
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selctedFragment).commit();
+                return true;
+
+            }
+        });
+
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+    public void mt(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        Log.d("tag", text);
+    }
+
+    public static void vibrate(Fragment fragment) {
+
+        Vibrator vibrator = (Vibrator) fragment.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            vibrator.vibrate(50);
+        }
+
+    }
+
+    void vibrateSense() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(50);
+        }
     }
 }
